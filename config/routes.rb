f0ba6ec8eb6f2a013a6c814ccb5fee7dfe634341
y.kondo <y.kondo@pepabo.com>
2017-06-05 20:11:34 +0900

    rootを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
+  root 'pages#index'
   get 'pages/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
