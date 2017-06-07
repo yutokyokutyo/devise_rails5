236136cb43c4112948f891237aa6cfb7e6b6e7bb
y.kondo <k.yuto.cre@gmail.com>
2017-06-07 15:53:03 +0900

    コントローラーとモデルの調整

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def line
+    basic_action
+  end
+
+  private
+
+  def basic_action
+    @omniauth = request.env['omniauth.auth']
+    if @omniauth.present?
+      @profile = SocialProfile.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+      if @profile
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile.user)
+      else
+        @profile = SocialProfile.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
+        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : Faker::Internet.email
+        @profile.user = current_user || User.create!(email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile.user)
+        redirect_to edit_user_path(@profile.user.id) and return
+      end
+    end
+    redirect_to root_path
   end
 end
--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -1,3 +1,20 @@
 class SocialProfile < ApplicationRecord
   belongs_to :user
+
+  def set_values(omniauth)
+    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
+    credentials = omniauth['credentials']
+    info = omniauth['info']
+
+    self.access_token = credentials['refresh_token']
+    self.access_secret = credentials['secret']
+    self.credentials = credentials.to_json
+    self.name = info['name']
+    self.set_values_by_raw_info(omniauth['extra']['raw_info'])
+  end
+
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
+  end
 end
